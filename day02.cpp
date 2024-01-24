ba2d8d9caaad5274c5f02d2464c3142c3c84bbe6
white9812 <white9812@gmail.com>
2024-01-24 13:20:39 +0900

    p.141 program 3-29

--- day02.cpp
+++ day02.cpp
@@ -7,19 +7,14 @@ using namespace std; // std::cout 에서 std::생략하려면 쓰는 것
 int main(){
    
    // 변수 선언
-   double number ;
-   int intpart ;
-   double frackPart ;
-   // 입력받기
-   cout << "부동 소수점 입력 : ";
-   cin >> number ;
-   //처리
-    intpart = static_cast<int>(number);
-    frackPart=number-intpart;
-    cout << fixed << showpoint << setprecision(2);
-    cout << "원래 값: " << number << endl ;
-    cout << "정수부분: "<< intpart << endl ;
-    cout << "소수점 아래 부분: " << frackPart << endl;
-    
+    unsigned int givenInt,firstDigit;
+  //입력받기
+    cout <<"양의 정수 입력: ";
+    cin >>givenInt;
+    firstDigit=givenInt %10 ;
+    cout << "입력한 정수: "<<givenInt << endl;
+    cout << "첫번째 자리수 추출 : "<< firstDigit << endl;
+
+
     return 0;
 }
