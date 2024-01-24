595220cf7fc529c9da923e35e0943f616d3e7521
white9812 <white9812@gmail.com>
2024-01-24 13:06:10 +0900

    조건부 표현식

--- day02.cpp
+++ day02.cpp
@@ -5,27 +5,16 @@ using namespace std; // std::cout 에서 std::생략하려면 쓰는 것
 
 
 int main(){
-    int day;
-    cout <<"Enter a nember between 0 and 6:";
-    cin >> day;
-    switch(day){
-        case 0: cout << "Sunday" << endl;
-                break;
-        case 1: cout << "Monday" << endl;
-                 break;
-        case 2: cout << "Tuesday" << endl;
-                 break;
-        case 3: cout << "Wednesday" << endl;
-                 break;
-        case 4: cout << "Thursday" << endl;
-                 break;
-        case 5: cout << "Friday" << endl;
-                 break;
-        case 6: cout << "Saturday" << endl;
-                 break;
-        // break가 없으면 입력한 숫자부터 그 다음수들까지 모두 출력됨
-        //case1부터 맞는 case인지 판단하고 맞는게 있으면 그다음 case도 처리하는것
-        default : cout<<"There is no days" <<endl;
-    }
+    int num1,num2;
+    int larger;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    larger = num1>=num2 ? num1:num2;
+    cout << "더 큰 숫자= " << larger << endl ;
+
+
     return 0;
 }
