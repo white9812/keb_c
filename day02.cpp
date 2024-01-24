20bf7d9d97d96d261883032f4d839315adfe8337
white9812 <white9812@gmail.com>
2024-01-24 11:33:50 +0900

    p.158

---
+++ day02.cpp
@@ -0,0 +1,12 @@
+//p.158
+#include <iostream> // cout 쓰기위해서
+#include <iomanip>;
+using namespace std; // std::cout 에서 std::생략하려면 쓰는 것
+
+int main(){
+    int x=1;
+    if (x==0)
+    cout << "abcde1";
+    cout << "abcde2";
+    return 0 ;
+}
