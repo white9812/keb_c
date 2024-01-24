d07d5b55880fd8e6042aa874acc47cb2ef1563f9
white9812 <white9812@gmail.com>
2024-01-24 12:46:09 +0900

    switch

--- day02.cpp
+++ day02.cpp
@@ -1,13 +1,30 @@
-//p.158
+
 #include <iostream> // iostream의 객체 cin(파이썬의 input()),cout 쓰기위해서
 #include <iomanip>;
 using namespace std; // std::cout 에서 std::생략하려면 쓰는 것
 
-int main(){
-    int x=1;
-    if (x==0)
-    cout << "abcde1";
-    cout << "abcde2";
-    return 0 ;
-}
 
+int main(){
+    int day;
+    cout <<"Enter a nember between 0 and 6:";
+    cin >> day;
+    switch(day){
+        case 0: cout << "Sunday" << endl;
+                break;
+        case 1: cout << "Monday" << endl;
+                 break;
+        case 2: cout << "Tuesday" << endl;
+                 break;
+        case 3: cout << "Wednesday" << endl;
+                 break;
+        case 4: cout << "Thursday" << endl;
+                 break;
+        case 5: cout << "Friday" << endl;
+                 break;
+        case 6: cout << "Saturday" << endl;
+                 break;
+        // break가 없으면 입력한 숫자부터 그 다음수들까지 모두 출력됨
+        //case1부터 맞는 case인지 판단하고 맞는게 있으면 그다음 case도 처리하는것
+   
+    }
+}
