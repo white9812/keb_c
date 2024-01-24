3541da35f5a628e9a02b729d7dea57468ca6aef0
white9812 <white9812@gmail.com>
2024-01-24 13:15:38 +0900

    p.138 program 3-28

--- day02.cpp
+++ day02.cpp
@@ -5,16 +5,21 @@ using namespace std; // std::cout 에서 std::생략하려면 쓰는 것
 
 
 int main(){
-    int num1,num2;
-    int larger;
-
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter the second number: ";
-    cin >> num2;
-    larger = num1>=num2 ? num1:num2;
-    cout << "더 큰 숫자= " << larger << endl ;
-
-
+   
+   // 변수 선언
+   double number ;
+   int intpart ;
+   double frackPart ;
+   // 입력받기
+   cout << "부동 소수점 입력 : ";
+   cin >> number ;
+   //처리
+    intpart = static_cast<int>(number);
+    frackPart=number-intpart;
+    cout << fixed << showpoint << setprecision(2);
+    cout << "원래 값: " << number << endl ;
+    cout << "정수부분: "<< intpart << endl ;
+    cout << "소수점 아래 부분: " << frackPart << endl;
+    
     return 0;
 }
