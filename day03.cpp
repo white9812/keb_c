3ce15c51f565a117571a6bac55287f944c1e6a51
white9812 <white9812@gmail.com>
2024-01-25 13:02:20 +0900

    6-4

--- day03.cpp
+++ day03.cpp
@@ -3,12 +3,29 @@
 using namespace std;
 
 int main(){
+    //변수 선언
     const double PI =3.141592653589793238462;
-    double degree = PI/4;
-    //45도의 sin,cos,tan 값 찾기
-    cout << "sin(45): "<<sin(degree)<<endl;
-    cout << "cos(45): "<<cos(degree) << endl;
-    cout <<"tan(45): "<<tan(degree);
-    return 0;
+    int n;
+    double s, peri,area;
+    //변의 개수 입력받기
+    do {
+        cout << "변의 개수를 입력하세요(4이상의 정수): ";
+        cin >> n;
+        }while(n<4);
+        //변의 길이 입력받기
+        do {
+            cout << "변의 길이를 입력하세요: ";
+            cin>> s;
 
-}
+
+        }while(s<=0.0);
+        //둘레와 넓이 구하기
+        peri=n*s;
+        area=n*pow(s,2)/(n*tan(PI/n));
+        //결과 출력
+        cout <<"둘레: "<<peri<<endl;
+        cout << "넓이: "<<area;
+        return 0;
+        }
+
+    
