2d6fad612f77b133ec0d644e4ae799bdb6b16133
white9812 <white9812@gmail.com>
2024-01-29 13:40:34 +0900

    p.442 program 8-14

--- day05.cpp
+++ day05.cpp
@@ -1,9 +1,43 @@
-/*이렇게 하나씩 체크하는건 시간이 많이 걸림*/
-int access(int number[5],int index,int capacity){
-    if (0 > index && index >capacity){
-        assert(false);  
-    }
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
 
-    return number[index];
+int main(){
+    //선언과 초기화
+    const int CAPACITY= 10;
+    int frequencies[CAPACITY]={0};
+    ifstream integerFile;
+    //정수 파일 열기
+    integerFile.open("integerFile.dat");
+    if(!integerFile){
+        cout <<"숫자 파일을 열 수 없습니다."<<endl;
+        cout << "프로그램을 중단합니다.";
+        return 0;
+    }
+    // 파일에서 정수를 읽어 들이고, 빈도 배열 생성
+    int data;
+    int size=0;
+    while(integerFile >> data){
+        if(data>=0 && data<=9){
+            size++;
+            frequencies[data]++;
+
+        }
+    }
+    // 정수 파일 닫기
+    integerFile.close();
+    // 빈도 배열과 히스토그램 출력
+    //cout << "파일 안에 " <<size <<"개의 유효한 데이터가 있습니다."<<endl;
+    cout << "파일 안에 " <<size <<"개의 유효한 데이터가 있습니다."<<endl;
+    for(int i=0;i<10;i++){
+        cout <<setw(3)<<i<<" ";
+        for(int f=1;f<=frequencies[i];f++){
+            cout<< "*";
+
+        }
+        cout<<" "<<frequencies[i]<<endl;
+    }
+    return 0;
 
 }
